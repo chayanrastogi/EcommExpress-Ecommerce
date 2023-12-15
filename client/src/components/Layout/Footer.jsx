4426a54b5d1be64d8e4c99714e3c47049318e581
Chayan Rastogi <cp.chayanrastogi@gmail.com>
2023-12-15 16:55:47 +0530

    .

---
+++ client/src/components/Layout/Footer.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Footer = () => {
+    return (
+        <>
+            <div className='bg-gray-950 text-white p-3'>
+                <h4 className='text-center'>
+                    All Rights Reserved &copy; <a href='https://chayanrastogi.github.io/Personal-Portfolio/' target='blank'><u>Chayan Rastogi</u></a>
+                </h4>
+            </div>
+        </>
+    )
+}
+
+export default Footer
---
+++ client/src/components/Layout/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <>
+      <div className=''>
+        <h1>
+          Header
+        </h1>
+      </div>
+    </>
+  )
+}
+
+export default Header
